72ed61e01b4d51026f2b28d741744bc781d34347
Erin-Reiraku <ss_erika@outlook.com>
2025-06-04 01:15:27 +1000

    Added overlay when menu activates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const menu   = document.querySelector('.mobile-menu');
 const hamburger = document.querySelector('.hamburger');
 const close  = document.querySelector('.mobile-close');
+const mobileNav = document.querySelector('.mobile-nav');
+const overlay    = document.querySelector('.menu-overlay');
 
 /* handle mobile menu display */
 hamburger.addEventListener('click', () => {
   menu.classList.add('is-open');
+  overlay.classList.add('is-active');
   hamburger.style.display = 'none';
   close.style.display  = 'block';
 });
@@ -12,6 +15,7 @@ hamburger.addEventListener('click', () => {
 /* handle close button on mobile menu display */
 close.addEventListener('click', () => {
   menu.classList.remove('is-open');
+  overlay.classList.remove('is-active');
   close.style.display  = 'none';
   hamburger.style.display = ''; // reset control to media
 });
@@ -21,7 +25,21 @@ const desktop = window.matchMedia('(min-width:1025px)');
 desktop.addEventListener('change', e => {
   if(e.matches){                 // already change to big screen
     menu.classList.remove('is-open');
+    overlay.classList.remove('is-active');
     close.style.display  = 'none';
     hamburger.style.display = '';   // reset control to media
+  }
+});
+
+/* handle click on outside moile menu will first close it */
+document.addEventListener('click', (e) => {
+  if ( menu.classList.contains('is-open') &&
+        !menu.contains(e.target) &&
+        !hamburger.contains(e.target) &&
+        !mobileNav.contains(e.target) ) {
+    menu.classList.remove('is-open');
+    overlay.classList.remove('is-active');
+    close.style.display  = 'none';
+    hamburger.style.display = '';
   }
 });
