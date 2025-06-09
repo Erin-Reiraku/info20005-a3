a569a2607c4745983dedecf56caaf82d0c66df4d
Erin-Reiraku <ss_erika@outlook.com>
2025-06-09 13:29:23 +1000

    Finished homepage with adding indicators in hero section

--- js/footer.js
+++ js/footer.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     <!-- MOBILE BOTTOM NAV -->
     <nav class="mobile-nav">
       <a href="#">
+        <img src="assets/icons/ui/account.svg" alt="Account">
+      </a>
+      <a href="#">
         <img src="assets/icons/ui/search.svg" alt="Search">
       </a>
       <a href="cart.html">
         <img src="assets/icons/ui/shopping_cart.svg" alt="Cart">
         <span class="dot">1</span>
-      </a>
-      <a href="#">
-        <img src="assets/icons/ui/account.svg" alt="Account">
       </a>
     </nav>
   `;
